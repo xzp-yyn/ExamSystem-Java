f1eb203cf9b7586149e3a32bc6b9ea14a25f3b4b
xuezhanpeng <3210625646@qq.com>
2023-03-15 17:59:04 +0800

    init

---
+++ src/main/java/com/xzp/ExamDataVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.xzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamDataVisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamDataVisualizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xzp/ExamDataVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamDataVisualizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
